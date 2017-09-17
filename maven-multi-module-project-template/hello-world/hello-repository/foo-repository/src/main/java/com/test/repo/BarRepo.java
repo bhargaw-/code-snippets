1c12a64df6576b4a4c9babe74d3c7aa34a1eebff
bhargaw- <bhargaw@hotmail.co.in>
2017-09-18 01:31:34 +0530

    maven multi module project template

---
+++ maven-multi-module-project-template/hello-world/hello-repository/foo-repository/src/main/java/com/test/repo/BarRepo.java
@@ -0,0 +1,7 @@
+package com.test.repo;
+
+import com.test.Bar;
+
+public class BarRepo {
+	Bar bar = new Bar();
+}
---
+++ maven-multi-module-project-template/hello-world/hello-repository/foo-repository/src/main/java/com/test/repo/FooRepo.java
@@ -0,0 +1,10 @@
+package com.test.repo;
+
+import com.test.Foo;
+
+public class FooRepo {
+	
+	Foo foo = new Foo();
+	
+
+}
